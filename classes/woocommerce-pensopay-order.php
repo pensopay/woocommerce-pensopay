131d8baed61d7af0ff7f4de99fb56b2e91804bbc
Dimitris Preketes <d.preketes@codecave.eu>
2020-12-09 17:10:57 +0200

    Fixes on calculation to account for VAT + discounts

--- classes/woocommerce-pensopay-order.php
+++ classes/woocommerce-pensopay-order.php
@@ -506,7 +506,7 @@ class WC_PensoPay_Order extends WC_Order {
 			'qty'        => $line_item->get_quantity(),
 			'item_no'    => $line_item->get_product_id(),
 			'item_name'  => $line_item->get_name(),
-			'item_price' => $line_item->get_total(),
+			'item_price' => ($line_item->get_total('edit') / $line_item->get_quantity()) + ($line_item->get_total_tax('edit') / $line_item->get_quantity()),
 //			'item_price' => wc_get_price_including_tax( $line_item->get_product() ),
 			'vat_rate'   => $vat_rate,
 		];
